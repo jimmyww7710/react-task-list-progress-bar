e531c4453c91ce788c03d4df93c82d1175146271
jimmyww7710 <36302780+jimmyww7710@users.noreply.github.com>
2022-10-16 23:22:46 +0800

    add 'progressedPercent' that display percentage of progress
    - function order to successfully get variable.
    - limit to started date only can set before ended date, and  ended date should be set later than started date.

--- src/component/task.js
+++ src/component/task.js
@@ -3,25 +3,55 @@ import { StyledTask } from '../style/task.styled'
 import Details from './details'
 
 function Task({title, startDate, endDate, items, order}) {
+    let wholeDays = 0;
+    let progressPercent = 0;
+    const currentDate = new Date();
+    const passedDays = Math.ceil(((currentDate - new Date(startDate)) / (1000 * 60 * 60 * 24)));
+    
+    const updateWholeDays = function(startDate, endDate) {
+      const _startDate = new Date(startDate);
+      const _endDate = new Date(endDate);
+      wholeDays = Math.ceil((_endDate - _startDate) / (1000 * 60 * 60 * 24)) + 1;
+      progressPercent = Math.floor((passedDays / wholeDays) * 100);
+      progressPercent = progressPercent > 100 ? 100 : progressPercent;
+    }
+
+   
     const[taskTitle, setTaskTitle] = useState(title);
     const updateTitle = function(e) {
         setTaskTitle(e.target.value);
     }
 
     const[taskStartDate, setTaskStartDate] = useState(startDate);
+    
+    const[taskEndDate, setTaskEndDate] = useState(endDate);
+
+    updateWholeDays(taskStartDate, taskEndDate);
     const updateStartDate = function(e) {
-        setTaskStartDate(e.target.value);
+        if (new Date(e.target.value) < taskEndDate) {
+          setTaskStartDate(e.target.value);
+          updateWholeDays(taskStartDate, taskEndDate);
+        }
+        else {
+          alert("Ended Date should be later than Started Date");
+        }
+        
     }
 
-    const[taskEndDate, setTaskEndDate] = useState(endDate);
     const updateTaskEndDate= function(e) {
+      if (new Date(e.target.value) > taskStartDate) {
         setTaskEndDate(e.target.value);
+        updateWholeDays(taskStartDate, taskEndDate);
+      }
+      else {
+        alert("Started Date should be earlier than Ended Date");
+      }
     }
     
     
     return (
       <StyledTask> 
-        <h2>Task {order}</h2>
+        <h2>Task {order}</h2>  {progressPercent} %
         <input type="text" value={taskTitle} onChange={updateTitle} />
         <input type="date" value={taskStartDate} onChange={updateStartDate}></input>
         <input type="date" value={taskEndDate} onChange={updateTaskEndDate}></input>
@@ -30,6 +60,7 @@ function Task({title, startDate, endDate, items, order}) {
             const {id, name, days} = item;
           return <Details key={id} name={name} days={days} ></Details>
         })}
+       
         
       </StyledTask>
     );
